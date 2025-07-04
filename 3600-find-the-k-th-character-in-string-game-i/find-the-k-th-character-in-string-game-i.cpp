1ca97bf55e02861f248e622cfd6b7e23e7b7fda5
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-04 23:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (90.89%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s="a";
-        while(s.size()<k){
-            int n=s.size();
-            for(int i=0;i<n;i++){
-                int val=s[i]-'a';
-                char ch=(char)('a'+val+1);
-                s.push_back(ch);
-            }
-        }
-        return s[k-1];
+        int shift=__builtin_popcount(k-1);
+        return 'a'+shift;
     }
 };
