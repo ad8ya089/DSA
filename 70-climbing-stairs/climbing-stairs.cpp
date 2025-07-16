8d04a76d504eddffcdd03619ec222c7ee0eb36c4
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-16 18:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    //Memoization
-    int f(int n,vector<int>&dp){
-        if(n<0) return 0;
-        if(n==0 or n==1) return 1;
-        if(dp[n]!=-1) return dp[n]; //overlapping subsequences
-        int one=f(n-1,dp);
-        int two=f(n-2,dp);
-        return dp[n]=one+two;
-    }
+    //Tabulation
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        int ans=f(n,dp);
-        return ans;
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
