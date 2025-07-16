4b820ddc114e119824853d8d140fd73bb00db251
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-16 20:51:12 +0530

    Time: 2 ms (7.59%) | Memory: 13.2 MB (5.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     //Memoization
     int f(int ind,vector<int>nums,vector<int>&dp){
-        if(ind<0) return 0;
         if(ind==0) return dp[ind]=nums[0];
         if(dp[ind]!=-1) return dp[ind];
-        int take=f(ind-2,nums,dp)+nums[ind];
+        int take=nums[ind];
+        if(ind>1) take+=f(ind-2,nums,dp);
         int notTake=f(ind-1,nums,dp)+0;
         return dp[ind]=max(take,notTake);
     }
