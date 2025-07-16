3cba890f6712713695603794cc14da9baff7d0bb
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-16 20:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (72.99%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    //Tabulation(Bottom-Up Approach)
+    //Tabulation(Bottom-Up Approach) with space optimisation
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
+        int prev2=0,prev1=nums[0];
         for(int ind=1;ind<n;ind++){
             int take=nums[ind];
-            if(ind>1) take+=dp[ind-2];
-            int notTake=0+dp[ind-1];
-            dp[ind]=max(take,notTake);
+            if(ind>1) take+=prev2;
+            int notTake=0+prev1;
+            int curr_i=max(take,notTake);
+            prev2=prev1;
+            prev1=curr_i;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
