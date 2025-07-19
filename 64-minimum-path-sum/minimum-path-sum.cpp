118756264db03e296aa1d7d19e2d56e82b2c2c87
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-19 14:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (40.70%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int m,n;
-    int f(int r,int c,vector<vector<int>>&dp,vector<vector<int>>& grid){
-        if(r==0 and c==0) return grid[0][0];
-        if(dp[r][c]!=-1) return dp[r][c];
-        int up=INT_MAX,left=INT_MAX;
-        if(r-1>=0) up=grid[r][c]+f(r-1,c,dp,grid);
-        if(c-1>=0) left=grid[r][c]+f(r,c-1,dp,grid);
-        return dp[r][c]=min(up,left);
-    }
+    //Tabulation
     int minPathSum(vector<vector<int>>& grid) {
-        m=grid.size();n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp,grid);
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                if(r==0 and c==0) dp[0][0]=grid[0][0];
+                else{
+                    int up=INT_MAX,left=INT_MAX;
+                    if(r-1>=0) up=dp[r-1][c];
+                    if(c-1>=0) left=dp[r][c-1];
+                    dp[r][c]=grid[r][c]+min(up,left);
+                }
+            }   
+        }
+        return dp[m-1][n-1];
     }
 };
