863e2bbdbb3f3aafaf1dc142f2c42a981be5baf2
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-19 13:31:53 +0530

    Time: 2 ms (18.71%) | Memory: 9.4 MB (33.81%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    //Memoization
-    int f(int r,int c,vector<vector<int>>&dp,int m,int n){
-        if(r<0 or c<0) return 0;
-        if(r==0 and c==0) return 1;
-        if(dp[r][c]!=-1) return dp[r][c];
-        int left=f(r,c-1,dp,m,n);
-        int up=f(r-1,c,dp,m,n);
-        return dp[r][c]=left+up;
-    }
+    //Tabulation
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp,m,n);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                if(r==0 and c==0) dp[r][c]=1;
+                else{
+                    int left=0,up=0;
+                    if(c-1>=0) left=dp[r][c-1];
+                    if(r-1>=0) up=dp[r-1][c];
+                    dp[r][c]=left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
