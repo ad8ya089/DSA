005406e7d024575e63002a60648669afe4b4b1b4
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-23 20:29:59 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (3.11%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int minMoves(int target, int maxDoubles) {
-        if(maxDoubles==0 or target==1) return target-1;
-        if(maxDoubles==1){
-            int moves=1;
-            if(target%2==0){
-                target/=2;
-            }
-            else{
-                moves++;
-                target--;
-                target/=2;
-            }
-            return target-1+moves;
-        }
         int moves=0;
-        while(target!=1){
-            if(target%2!=0 or maxDoubles==0){
+        while(target!=1 and maxDoubles){
+            if(target%2!=0){
                 target--;
                 moves++;
             }
@@ -26,6 +13,6 @@ public:
                 maxDoubles--;
             }
         }
-        return moves;
+        return moves+target-1;
     }
 };
