d446acf68adaf23290c4f6bbbbfaa575c0cb828d
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-25 11:20:39 +0530

    Time: 17 ms (62.13%) | Memory: 28.3 MB (54.53%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+    //Memoization
     int f(int ind,int target,vector<int>&coins,vector<vector<int>>&dp){
         if(target==0) return 1;
-        if(ind<0) return 0;
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int sum=0;
-        int take=0;
-        while(sum<=target){
-            take+=f(ind-1,target-sum,coins,dp);
-            sum+=coins[ind];
+        if(ind==0){
+            if(target%coins[0]==0) return 1;
+            return 0;
         }
-        return dp[ind][target]=take;
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        int notTake=f(ind-1,target,coins,dp);
+        int take=0;
+        if(target>=coins[ind]) take=f(ind,target-coins[ind],coins,dp); //take krne ke baad bhi same index pe reh laadle
+        return dp[ind][target]=take+notTake;
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        sort(coins.begin(),coins.end());
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
         return f(n-1,amount,coins,dp);
     }
