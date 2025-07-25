0d509840acf70b36de5a2deb785444bca0465200
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-25 17:41:18 +0530

    Time: 28 ms (41.41%) | Memory: 27.5 MB (51.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    //Memoization
-    int n1,n2;
-    int f(int ind1,int ind2,string &s1, string &s2,vector<vector<int>>&dp){
-        if(ind1==0 and ind2==0){
-            return s1[ind1]==s2[ind2];
-        }
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(ind1==0){
-            if(s1[ind1]==s2[ind2]) return 1;
-            return dp[ind1][ind2]=f(ind1,ind2-1,s1,s2,dp);
-        }
-        if(ind2==0){
-            if(s1[ind1]==s2[ind2]) return 1;
-            return dp[ind1][ind2]=f(ind1-1,ind2,s1,s2,dp);
-        }
-        if(s1[ind1]==s2[ind2]){
-            return dp[ind1][ind2]=1+f(ind1-1,ind2-1,s1,s2,dp);
-        }
-        else{
-            int one=f(ind1-1,ind2,s1,s2,dp);
-            int two=f(ind1,ind2-1,s1,s2,dp);
-            return dp[ind1][ind2]=max(one,two);
-        }
-        return dp[ind1][ind2]=0;
-    }
+    //Tabulation
     int longestCommonSubsequence(string s1, string s2) {
-        n1=s1.size();n2=s2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1)); //ind1,ind2 are the two states
-        return f(n1-1,n2-1,s1,s2,dp);
+        int n1=s1.size(),n2=s2.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0)); 
+        //n1+1,n2+1 liye instead of n1,n2 to incorporate base case of negative indices, smjha mc
+        //1-based indexing krdiye hai so string ka index uss hisab se ek-ek kam krdena
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2=1;ind2<=n2;ind2++){
+                if(s1[ind1-1]==s2[ind2-1]) dp[ind1][ind2]=1+dp[ind1-1][ind2-1]; //Match
+                else dp[ind1][ind2]=0+max(dp[ind1-1][ind2],dp[ind1][ind2-1]); //Not matching
+            }
+        }
+        return dp[n1][n2];
     }
 };
