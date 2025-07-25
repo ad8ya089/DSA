321a4bef76bd1bf9fed219dffa8be2d16941bac8
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-25 16:04:52 +0530

    Time: 8 ms (81.10%) | Memory: 10.3 MB (86.60%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    //Tabulation
+    //Tabulation with space optimisation to 1 array only
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<long>>dp(n,vector<long>(amount+1,0));
-        for(int ind=0;ind<n;ind++){
-            dp[ind][0]=1;
-        }
+        vector<long>prev(amount+1,0);
         for(int target=0;target<=amount;target++){
-            if(target%coins[0]==0) dp[0][target]=1;
+            if(target%coins[0]==0) prev[target]=1;
         }
         for(int ind=1;ind<n;ind++){
-            for(int target=1;target<=amount;target++){
-                int notTake=dp[ind-1][target];
+            for(int target=0;target<=amount;target++){
+                int notTake=prev[target];
                 int take=0;
-                if(target>=coins[ind]) take=dp[ind][target-coins[ind]]; //take krne ke baad bhi same index pe reh laadle
-                dp[ind][target]=(long)take+notTake;
+                if(target>=coins[ind]) take=prev[target-coins[ind]]; //take krne ke baad bhi same index pe reh laadle
+                prev[target]=(long)take+notTake;
             }
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
