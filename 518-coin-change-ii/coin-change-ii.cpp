5f70ec55594cf83184a4f56b879073de9b8d2a80
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-25 13:00:01 +0530

    Time: 31 ms (25.89%) | Memory: 45.3 MB (20.63%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    //Memoization
-    int f(int ind,int target,vector<int>&coins,vector<vector<int>>&dp){
-        if(target==0) return 1;
-        if(ind==0){
-            if(target%coins[0]==0) return 1;
-            return 0;
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int notTake=f(ind-1,target,coins,dp);
-        int take=0;
-        if(target>=coins[ind]) take=f(ind,target-coins[ind],coins,dp); //take krne ke baad bhi same index pe reh laadle
-        return dp[ind][target]=take+notTake;
-    }
+    //Tabulation
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
+        vector<long>prev(amount+1,0);
+        for(int target=0;target<=amount;target++){
+            if(target%coins[0]==0) prev[target]=1;
+        }
+        for(int ind=1;ind<n;ind++){
+            vector<long>curr(amount+1,0);
+            for(int target=0;target<=amount;target++){
+                int notTake=prev[target];
+                int take=0;
+                if(target>=coins[ind]) take=curr[target-coins[ind]]; //take krne ke baad bhi same index pe reh laadle
+                curr[target]=(long)take+notTake;
+            }
+            prev=curr;
+        }
+        return prev[amount];
     }
 };
