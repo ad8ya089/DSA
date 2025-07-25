b6ec340de3117f36271a9517b055b7ecfc140f2b
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-25 18:00:13 +0530

    Time: 12 ms (95.40%) | Memory: 9 MB (96.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    //Tabulation with space optimisation (2 arrays)
     int longestCommonSubsequence(string s1, string s2) {
+        //Space optimisation using 1 array and 1 variable (kyuki ek hi element of prev array pe depend kr rha tha jisko hum variable mei hi store krliye)
         int n1=s1.size(),n2=s2.size();
-        vector<int>prev(n2+1,0),curr(n2+1,0); 
+        vector<int>curr(n2+1,0);
+        int prev=0;
         //n1+1,n2+1 liye instead of n1,n2 to incorporate base case of negative indices, smjha mc
         //1-based indexing krdiye hai so string ka index uss hisab se ek-ek kam krdena
-        for(int ind1=1;ind1<=n1;ind1++){
-            for(int ind2=1;ind2<=n2;ind2++){
-                if(s1[ind1-1]==s2[ind2-1]) curr[ind2]=1+prev[ind2-1]; //Match
-                else curr[ind2]=0+max(prev[ind2],curr[ind2-1]); //Not matching
+        for(int i=1;i<=n1;i++){
+            prev=0;
+            for(int j=1;j<=n2;j++){
+                int prev1=curr[j];
+                if(s1[i-1]==s2[j-1]) curr[j]=1+prev;//Match
+                else curr[j]=0+max(curr[j],curr[j-1]); //Not Match
+                prev=prev1;
             }
-            prev=curr;
         }
-        return prev[n2];
+        return curr[n2];
     }
 };
