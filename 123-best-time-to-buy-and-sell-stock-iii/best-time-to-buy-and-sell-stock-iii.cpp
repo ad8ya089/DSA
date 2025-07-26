c869f3497e68f88f0f8e427c7cfced81a614a02c
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-26 16:26:44 +0530

    Time: 408 ms (42.50%) | Memory: 211.5 MB (41.80%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    //
-    int n;
-    int f(int ind,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>&dp){
-        if(cap==0) return 0;
-        if(ind==n) return 0;
-        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-        if(buy){
-            int take=-prices[ind]+f(ind+1,0,cap,prices,dp);
-            int notTake=0+f(ind+1,1,cap,prices,dp);
-            return dp[ind][buy][cap]=max(take,notTake);
-        }
-        else{
-            int sell=prices[ind]+f(ind+1,1,cap-1,prices,dp);
-            int notSell=0+f(ind+1,0,cap,prices,dp);
-            return dp[ind][buy][cap]=max(sell,notSell);
-        }
-    }
+    //Tabulation
     int maxProfit(vector<int>& prices) {
-        n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        //Nx2x3 buy-->1, sell->0
-        return f(0,1,2,prices,dp);
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        //(N+1)x2x3 buy-->1, sell->0 , N+1 to incorporate ind==n case in tabulation
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=2;cap>=1;cap--){
+                    if(buy){
+                        int take=-prices[ind]+dp[ind+1][0][cap];
+                        int notTake=0+dp[ind+1][1][cap];
+                        dp[ind][buy][cap]=max(take,notTake);
+                    }
+                    else{
+                        int sell=prices[ind]+dp[ind+1][1][cap-1];
+                        int notSell=0+dp[ind+1][0][cap];
+                        dp[ind][buy][cap]=max(sell,notSell);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
