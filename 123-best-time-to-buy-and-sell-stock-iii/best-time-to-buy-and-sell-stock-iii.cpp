e68a2f5feba51798e302e92154961eb0b06aed84
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-07-26 16:37:59 +0530

    Time: 35 ms (78.85%) | Memory: 79.3 MB (92.04%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        //Tabulation O(Nx4)
+        //Space optimisation O(Nx4)
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(5,0));
-        for(int i=0;i<=4;i++) dp[n][i]=0;
-        for(int i=0;i<=n;i++) dp[i][4]=0;
+        vector<int>ahead(5,0),curr(5,0);
+        for(int i=0;i<=4;i++) ahead[i]=0;
+        curr[4]=0;
         for(int ind=n-1;ind>=0;ind--){
             for(int transac=3;transac>=0;transac--){
                 if(transac%2==0){//Buy
-                    int take=-prices[ind]+dp[ind+1][transac+1];
-                    int notTake=0+dp[ind+1][transac];
-                    dp[ind][transac]=max(take,notTake);
+                    int take=-prices[ind]+ahead[transac+1];
+                    int notTake=0+ahead[transac];
+                    curr[transac]=max(take,notTake);
                 }
                 else{ //Sell
-                    int sell=prices[ind]+dp[ind+1][transac+1];
-                    int notSell=0+dp[ind+1][transac];
-                    dp[ind][transac]=max(sell,notSell);
+                    int sell=prices[ind]+ahead[transac+1];
+                    int notSell=0+ahead[transac];
+                    curr[transac]=max(sell,notSell);
                 }
             }
+            ahead=curr;
         }
-        return dp[0][0];
+        return ahead[0];
     }
 };
