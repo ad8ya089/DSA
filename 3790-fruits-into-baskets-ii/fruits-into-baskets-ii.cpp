1e89ca4b0825ca86b7b483cb781c1543eaaf266d
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-05 13:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (75.84%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -3,12 +3,11 @@ public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n=fruits.size();
         int ct=0;
-        set<int>st;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(!st.count(j) and baskets[j]>=fruits[i]){
-                    st.insert(j);
+                if(baskets[j]>=fruits[i]){
                     ct++;
+                    baskets[j]=-1;
                     break;
                 }
             }
