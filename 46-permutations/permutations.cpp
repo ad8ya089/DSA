8a56e3f953eaa8817f8ad6840ce55508537bbf65
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-11 00:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (67.98%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    //Backtracking using space(visited array)
+    //Backtracking without space
     int n;
-    void backtrack(int start,vector<vector<int>>&list,vector<int>&tempList,vector<int>&nums,vector<bool>&used)
+    void backtrack(int start,vector<vector<int>>&list,vector<int>&nums)
     {
-        if(tempList.size()==n){
-            list.push_back(tempList);
+        //start uss pointer pe hai jaha pe hum uske baad ke saare elements laa skte hai to create a permutation
+        if(start==n){
+            list.push_back(nums);
             return;
         }
-        for(int i=0;i<n;i++){
-            if(used[i]) continue;
-            used[i]=true;
-            tempList.push_back(nums[i]);
-            backtrack(i+1,list,tempList,nums,used);
-            used[i]=false;
-            tempList.pop_back();
+        for(int i=start;i<n;i++){
+            swap(nums[i],nums[start]); //swap 1->1, 1->2, 1->3, vaise hi 2->2,2->3 and 3->3
+            backtrack(start+1,list,nums); //pointer ko aage badhao na ki index ko
+            swap(nums[i],nums[start]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         n=nums.size();
         vector<vector<int>>list;
-        vector<int>tempList;
-        vector<bool>used(n,false);
-        backtrack(0,list,tempList,nums,used);
+        backtrack(0,list,nums);
         return list;
     }
 };
