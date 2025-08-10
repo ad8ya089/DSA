b1abe9aee15147240ef12073a23e09ea4b3e55b1
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-11 02:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (54.23%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
+    //Aryan's backtracking template (iske pehle wala submission was take/notTake recursion)
     int n;
-    void backtrack(int ind,vector<int>&candidates,int target,vector<vector<int>>&list,vector<int>&tempList){
+    void backtrack(int start,vector<int>&candidates,int target,vector<vector<int>>&list,vector<int>&tempList){
         if(target<0) return;
-        if(ind==n){
-            if(target==0){
-                list.push_back(tempList);
+        else if(target==0) list.push_back(tempList);
+        else{
+            for(int i=start;i<n;i++){
+                tempList.push_back(candidates[i]);
+                backtrack(i,candidates,target-candidates[i],list,tempList);
+                tempList.pop_back();
             }
-            return;
         }
-        //take (vhi reh kyuki ek ko kitni baar bhi le skte)
-        tempList.push_back(candidates[ind]);
-        backtrack(ind,candidates,target-candidates[ind],list,tempList);
-        //notTake (aage badh jaye bina liye)
-        tempList.pop_back();
-        backtrack(ind+1,candidates,target,list,tempList);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         n=candidates.size();
