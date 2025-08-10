790c100f9b21ae47312dc66a50a77de3f857a27c
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-10 14:18:08 +0530

    Time: 10 ms (51.99%) | Memory: 27.6 MB (94.66%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    //Tabulation
+    //Tabulation with space optimisation (2 array)
     int maximalSquare(vector<vector<char>>& mat) {
         int m=mat.size(),n=mat[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int>prev(n+1,0),curr(n+1,0);
         int ans=0;
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(mat[i-1][j-1]=='1'){
-                    dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
-                    ans=max(ans,dp[i][j]);
+                    curr[j]=min({prev[j],curr[j-1],prev[j-1]})+1;
+                    ans=max(ans,curr[j]);
+                }
+                else{
+                    curr[j]=0;
                 }
             }
+            prev=curr;
         }
         return ans*ans;       
     }
