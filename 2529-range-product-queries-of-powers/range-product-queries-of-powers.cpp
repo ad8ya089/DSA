2cd30b559609ed622d27bf8fa131c074171d7ebe
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-11 19:34:56 +0530

    Time: 21 ms (57.45%) | Memory: 147.7 MB (58.51%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
+    //better way to calculate binary representation
     const int M=1e9+7;
-    vector<int>binR(int n){
-        int ct=0;
-        vector<int>p;
-        while(n){
-            if(n%2==1) p.push_back(1<<ct);
-            ct++;
-            n/=2;
-        }
-        return p;
-    }
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> powers=binR(n);
+        vector<int> powers;
+        for(int i=0;i<32;i++){
+            if((n&(1<<i))!=0){ //ith bit is set
+                powers.push_back(1<<i);
+            }
+        }
         vector<int>ans;
         for(int i=0;i<queries.size();i++){
             int start=queries[i][0],end=queries[i][1];
