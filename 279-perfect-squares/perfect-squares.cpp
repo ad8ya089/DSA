5934031ccf6ab59f48202b026d8826961594784e
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-12 16:06:36 +0530

    Time: 35 ms (90.66%) | Memory: 13.2 MB (58.92%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    //1D DP (Memo)
+    //1D DP (Tabulation) --> Method 1
     vector<int>dp;
-    int solve(int n){
-        if(n<=0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int ans=1e9;
-        for(int i=1;i*i<=n;i++){
-            int sq=i*i;
-            ans=min(ans,1+solve(n-sq)); //take (sab case hi dekh rha na toh notTake auto hai)
-        }
-        return dp[n]=ans;
-    }
     int numSquares(int n) {
-        dp=vector<int>(n+1,-1);
-        return solve(n);
+        dp=vector<int>(n+1,0);
+        for(int i=1;i<=n;i++){
+            dp[i]=i; //max ans for any ith value is i itself
+            for(int j=1;j*j<=i;j++){
+                int sq=j*j;
+                dp[i]=min(dp[i],1+dp[i-sq]); 
+            }
+        }
+        return dp[n];
     }
 };
