8c43b922c67053187f80f9b56857295190289762
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-12 17:54:28 +0530

    Time: 91 ms (71.24%) | Memory: 73.2 MB (54.90%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-    //Memo(bina binary exponentiation ke ni hoga)
-    vector<vector<int>>dp;
-    int x;
+    //Tabulation-2D (bina binary exponentiation ke ni hoga)
     const int M=1e9+7;
     //Binary exponentiation template (CP template se uthaya)
-    int power(long long x,long long y){
-        int res=1;
-        while(y>0){
-            if(y&1) res=(res*x)%M;
-            y=y>>1;
-            x=(x*x)%M;
+    int power(long long base,long long exp){
+        long long res=1;
+        while(exp>0){
+            if(exp&1) res=(res*base)%M;
+            exp=exp>>1;
+            base=(base*base)%M;
         }
-        return res;
+        return (int)res;
     }
-    int f(int n,int curr){
-        if(n==0) return 1;
-        if(curr>n) return 0; //n<0 pe bhi
-        if(dp[n][curr]!=-1) return dp[n][curr];
-        int val=power(curr,x);
-        if(val>n) return 0; //time ki bachat lol
-        int take=0;
-        if(n>=val) take=f(n-val,curr+1);
-        int notTake=f(n,curr+1);
-        return dp[n][curr]=(take+notTake)%M;
-    }
-    int numberOfWays(int n, int X) {
-        x=X;
-        dp=vector<vector<int>>(n+1,vector<int>(n+1,-1));
-        return f(n,1);
+    int numberOfWays(int n, int x) {
+        vector<int> pows; 
+        for (int i = 1;; i++) {
+            long long val = power(i, x);
+            if (val > n) break;
+            pows.push_back((int)val);
+        }
+        int m=pows.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0)); //{curr,target}
+        for(int curr=0;curr<=m;curr++){
+            dp[curr][0]=1;
+        }
+        for(int i=m-1;i>=0;i--){
+            for(int target=0;target<=n;target++){
+                //not take
+                dp[i][target]=dp[i+1][target];
+                //take current number
+                if(target>=pows[i]){
+                    dp[i][target]=(dp[i][target]+dp[i+1][target-pows[i]])%M;
+                }
+            }
+        }
+        return dp[0][n];
     }
 };
