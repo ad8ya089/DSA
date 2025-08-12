ab98aa17aaef4164dc8dc7a6cb808ccf1bfeebe2
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-12 16:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.44%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    //1D DP (Tabulation) --> Method 2
-    vector<int>dp;
+    //Langrange's 4 square theorem 
+    //Adrien-Marie Legendre 3-square theorem 
     int numSquares(int n) {
-        dp=vector<int>(n+1,1e9); //take all max values to be 1e9
-        dp[0]=0; //n==0 pe return 0 --> base case
-        for(int i=1;i<=n;i++){
-            for(int j=1;j*j<=i;j++){
-                int sq=j*j;
-                dp[i]=min(dp[i],1+dp[i-sq]); 
+        int val=sqrt(n);
+        if(val*val==n) return 1; //n khud hi perfect square hai
+        while(n%4==0){ //4^k(8m+7) ke form mei hoga tbhi 4 honge integer squares
+            n/=4;
+        }
+        if(n%8==7) return 4;
+        // n ko saare 4 se divide krke 3 squares hone ka possibility khtm ni kr dete just that scale jo horha tha 2^2 se har integer square toh voh hat gya and ab sb integers jiske squares se ban rha hai voh aapas mei 2 GCD ni rkhte hai, like 324=4*81 , toh 324 aur 81 dono hi square toh rhenge hi naa irrespective of 4 hataye ya ni 
+        for(int i=1;i*i<=n;i++){ //sum of 2 perfect squares hai ya ni
+            int sq=i*i;
+            int rem=(int)sqrt(n-sq);
+            if(rem*rem==n-sq){
+                return 2;
             }
         }
-        return dp[n];
+        return 3;
     }
 };
