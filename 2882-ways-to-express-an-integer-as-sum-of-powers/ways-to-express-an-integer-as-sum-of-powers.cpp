046cb445adb0247f0d8d7e3339dcc9e4e855ba39
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-12 17:21:27 +0530

    Time: 246 ms (44.44%) | Memory: 274.5 MB (28.10%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    //Memo
+    vector<vector<int>>dp;
+    int x;
+    const int M=1e9+7;
+    //Binary exponentiation template (CP template se uthaya)
+    int power(long long x,long long y){
+        int res=1;
+        while(y>0){
+            if(y&1) res=(res*x)%M;
+            y=y>>1;
+            x=(x*x)%M;
+        }
+        return res;
+    }
+    int f(int n,int curr){
+        if(n==0) return 1;
+        if(curr>n) return 0; //n<0 pe bhi
+        if(dp[n][curr]!=-1) return dp[n][curr];
+        int val=power(curr,x);
+        int take=0;
+        if(n>=val) take=f(n-val,curr+1);
+        int notTake=f(n,curr+1);
+        return dp[n][curr]=(take+notTake)%M;
+    }
+    int numberOfWays(int n, int X) {
+        x=X;
+        dp=vector<vector<int>>(n+1,vector<int>(n+1,-1));
+        return f(n,1);
+    }
+};
