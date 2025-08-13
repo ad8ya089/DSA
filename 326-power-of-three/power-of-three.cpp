6cef62848353184669f15f14400a056a92a296a4
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-13 18:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0) return false;
-        while(n%3==0){
-            n/=3;
-        }
-        if(n==1) return true;
-        return false;
+        if(n<=0) return false;
+        double x=log10(n)/log10(3);
+        return x==(int)x;
     }
 };
