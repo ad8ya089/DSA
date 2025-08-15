2159ef58f07316d4fddc37a618d857fd681e7886
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-15 13:07:43 +0530

    Time: 2 ms (5.23%) | Memory: 7.9 MB (18.14%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0) return false;
-        while(n%4==0){
-            n/=4;
-        }
-        return n==1;
+        double x=log10(n)/log10(4);
+        return x==(int)x;
     }
 };
