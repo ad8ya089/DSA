70137d7ebc2f0f13298aa22886867172a3b24a4f
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-24 15:49:20 +0530

    Time: 20 ms (53.40%) | Memory: 36.7 MB (59.58%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,45 +3,36 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
         vector<vector<int>> ans(n,vector<int>(m,INT_MAX));
-        queue<pair<pair<int,int>,int>>q;
+        queue<pair<int,int>>q;
         vector<vector<bool>>vis(n,vector<bool>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    q.push({{i,j},0});
-                    ans[i][j]=0;
+                    q.push({i,j});
                     vis[i][j]=1;
+                    ans[i][j]=0;
                 }
             }
         }
-        
+        int dist=1;
         vector<int>dr={-1,0,0,1};
         vector<int>dc={0,-1,1,0};
-        while(!q.empty())
-        {
-            auto it=q.front();
-            q.pop();
-            int x=it.first.first;
-            int y=it.first.second;
-            int d=it.second;
-            for(int k=0;k<4;k++)
-            {
-                int nr=x+dr[k];
-                int nc=y+dc[k];
-                if(nr>=0 and nc>=0 and nr<n and nc<m  and mat[nr][nc]==1)
-                {
-                    if(ans[nr][nc]>(d+1))
-                    {
-                        q.push({{nr,nc},d+1});
-                        ans[nr][nc]=min(ans[nr][nc],d+1);
-
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                int r=q.front().first,c=q.front().second;
+                q.pop();
+                for(int k=0;k<4;k++){
+                    int nr=r+dr[k];
+                    int nc=c+dc[k];
+                    if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc] and mat[nr][nc]!=0){
+                        vis[nr][nc]=1;
+                        q.push({nr,nc});
+                        ans[nr][nc]=min(ans[nr][nc],dist);
                     }
-                    
-                    
                 }
             }
-
-
+            dist++;
         }
         return ans;
     }
