dfe81615d9c2cd1fd1b60b7919d73ce3f6a7aa29
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-27 00:38:32 +0530

    Time: 14 ms (77.57%) | Memory: 26.3 MB (34.80%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
+    //djikstra se toh hogya ab dequeue se krle (0-1 BFS)
     int minCost(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<int>dx={0,0,1,-1};
         vector<int>dy={1,-1,0,0};
         vector<vector<int>>cost(m,vector<int>(n,INT_MAX));
-        //MinHeap
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-        pq.emplace(0,0,0);
+        //Deque
+        deque<pair<int,int>>dq;
+        dq.emplace_front(0,0);
         cost[0][0]=0;
-        while(!pq.empty()){
-            auto [currCost,x,y]=pq.top();pq.pop();
-            if(currCost>cost[x][y]) continue;
+        while(!dq.empty()){
+            auto [x,y]=dq.front();dq.pop_front();
             for(int dir=0;dir<4;dir++){
                 int nx=x+dx[dir];
                 int ny=y+dy[dir];
                 if(nx<0 or nx>=m or ny<0 or ny>=n) continue;
-                int nextCost=currCost +(dir!=grid[x][y]-1);
+                int nextCost=cost[x][y] +(dir!=grid[x][y]-1);
                 if(nextCost<cost[nx][ny]){
                     cost[nx][ny]=nextCost;
-                    pq.emplace(nextCost,nx,ny);
+                    if(dir==grid[x][y]-1){
+                        dq.emplace_front(nx,ny); // 0-cost edge
+                    }
+                    else{
+                        dq.emplace_back(nx,ny);
+                    }
                 }
             } 
         }
