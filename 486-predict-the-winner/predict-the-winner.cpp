e843b6c76d4894623009d7ea09bb59f189f9641e
ad8ya089 <87231628+ad8ya089@users.noreply.github.com>
2025-08-28 20:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (47.53%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    //Basic recursion --> Brute force --> O(2^n)
+    //Memoization (Top-Down) --> O(n^2)
     int n;
-    int maxDiff(vector<int>&nums,int left,int right){
+    int maxDiff(vector<int>&nums,int left,int right,vector<vector<int>>&dp){
         if(left==right){
             return nums[left];
         }
-        int scoreL=nums[left]-maxDiff(nums,left+1,right);
-        int scoreR=nums[right]-maxDiff(nums,left,right-1);
-        return max(scoreL,scoreR);
+        if(dp[left][right]!=-1) return dp[left][right];
+        int scoreL=nums[left]-maxDiff(nums,left+1,right,dp);
+        int scoreR=nums[right]-maxDiff(nums,left,right-1,dp);
+        return dp[left][right]=max(scoreL,scoreR);
     }
     bool predictTheWinner(vector<int>& nums) {
         n=nums.size();
-        int mxD=maxDiff(nums,0,n-1);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int mxD=maxDiff(nums,0,n-1,dp);
         return mxD>=0;
     }
 };
